5bccf2db6e35c1a85cdcbba90fab6fa0722c1874
Coslate <coslate@media.ee.ntu.edu.tw>
2020-06-01 01:52:00 +0800

    Initialize HW4.

---
+++ HW4/Hw4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <errno.h>
+#include <math.h>
+#include <time.h>
+#include <pthread.h>
+
+
+
+int main(){
+
+
+ 
+    return EXIT_SUCCESS;
+}
